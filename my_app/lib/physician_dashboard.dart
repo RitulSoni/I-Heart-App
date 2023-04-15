4a71c9d8fd03c6d39b9ae7a56ca6781fee20e6ed
Ritul Soni <rsoni27@illinois.edu>
2023-04-15 02:30:08 -0500

    changed button style

--- my_app/lib/physician_dashboard.dart
+++ my_app/lib/physician_dashboard.dart
@@ -4,6 +4,20 @@ import 'main.dart'; // Import MainPage from main.dart
 class PhysicianDashboard extends StatelessWidget {
   const PhysicianDashboard({Key? key}) : super(key: key);
 
+  // Helper method to create custom buttons
+  Widget _customButton(BuildContext context, String text, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: Theme.of(context).primaryColor,
+        onPrimary: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+      ),
+      child: Text(text, style: const TextStyle(fontSize: 18)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,37 +28,25 @@ class PhysicianDashboard extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement create new patient profile functionality
-              },
-              child: const Text('Create New Patient Profile'),
-            ),
+            _customButton(context, 'Create New Patient Profile', () {
+              // TODO: Implement create new patient profile functionality
+            }),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement view patient profiles functionality
-              },
-              child: const Text('View Patient Profiles'),
-            ),
+            _customButton(context, 'View Patient Profiles', () {
+              // TODO: Implement view patient profiles functionality
+            }),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement view CHD library functionality
-              },
-              child: const Text('View CHD Library'),
-            ),
+            _customButton(context, 'View CHD Library', () {
+              // TODO: Implement view CHD library functionality
+            }),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to MainPage (Model Viewer) when pressing the Demo Mode button
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainPage(title: 'Model Viewer Demo')),
-                );
-              },
-              child: const Text('Demo Mode'),
-            ),
+            _customButton(context, 'Demo Mode', () {
+              // Navigate to MainPage (Model Viewer) when pressing the Demo Mode button
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MainPage(title: 'Model Viewer Demo')),
+              );
+            }),
           ],
         ),
       ),
